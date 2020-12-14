472c1127d5d4ec03d4a5050d7fcc4a58ff6b9c9a
s-a-v-a-n-n-a <70327418+s-a-v-a-n-n-a@users.noreply.github.com>
2020-12-14 16:04:33 +0300

    Tree functions added

--- diff_tree.h
+++ diff_tree.h
@@ -46,11 +46,12 @@ typedef enum tree_operations_names
     OP_SIN,
     OP_COS,
     OP_LN,
+    OP_SQRT,
     OP_DIFF,
     NO_OP
 }tree_operations;
 
-extern const int tree_priorities[8];
+extern const int tree_priorities[10];
 
 extern const char* tree_operations_texts[];
 
@@ -66,6 +67,8 @@ struct Diff_tree_node
     double number;
     char variable;
     tree_operations operation;
+
+    long long depth;
 
     long long next;
 };
@@ -95,50 +98,57 @@ typedef enum tree_functions_identifiers
 
 extern const char* TREE_FUNCTION_IDENTIFIERS[];
 
-Diff_tree*       tree_init(const long long amount);
-void             tree_delete(Diff_tree* dtree);
-tree_code        tree_construct(Diff_tree* dtree, const long long amount);
-void             tree_destruct(Diff_tree* dtree);
+Diff_tree*       tree_init                          (const long long amount);
+void             tree_delete                        (Diff_tree* dtree);
+tree_code        tree_construct                     (Diff_tree* dtree, const long long amount);
+void             tree_destruct                      (Diff_tree* dtree);
 
-void tree_copy_node(Diff_tree* copy_from, Diff_tree* copy_to, long long root_to_copy, long long* where_to_copy);
-void tree_copy_branch(Diff_tree* copy_from, Diff_tree* copy_to, long long root_to_copy, long long where_to_copy);
+void             tree_copy_node                     (Diff_tree* copy_from, Diff_tree* copy_to, long long root_to_copy, long long* where_to_copy);
+void             tree_copy_branch                   (Diff_tree* copy_from, Diff_tree* copy_to, long long root_to_copy, long long where_to_copy);
 
-long long        tree_parent(Diff_tree* dtree, long long index);
-long long        tree_right_son(Diff_tree* dtree, long long index);
-long long        tree_left_son(Diff_tree* dtree, long long index);
+long long        tree_parent                        (Diff_tree* dtree, long long index);
+long long        tree_right_son                     (Diff_tree* dtree, long long index);
+long long        tree_left_son                      (Diff_tree* dtree, long long index);
 
-int              tree_if_lief(Diff_tree* dtree, long long index);
+long long        tree_go_up                         (Diff_tree* dtree, long long index, long long steps_amount);
 
-tree_code        tree_resize(Diff_tree* dtree, const double coefficient);
+char             tree_get_type                      (Diff_tree* dtree, long long index);
+tree_operations  tree_get_operation                 (Diff_tree* dtree, long long index);
+double           tree_get_number                    (Diff_tree* dtree, long long index);
+char             tree_get_variable                  (Diff_tree* dtree, long long index);
 
-//long long        tree_search(Diff_tree* dtree, const char* message);
+long long        tree_get_depth                     (Diff_tree* dtree, long long index);
 
-tree_code tree_check_index(Diff_tree* dtree, long long index);
+int              tree_if_lief                       (Diff_tree* dtree, long long index);
+
+tree_code        tree_resize                        (Diff_tree* dtree, const double coefficient);
+
+tree_code        tree_check_index                   (Diff_tree* dtree, long long index);
  
-tree_code tree_change_operation(Diff_tree* dtree, long long index, tree_operations operation);
+tree_code        tree_change_operation              (Diff_tree* dtree, long long index, tree_operations operation);
 
-tree_code        tree_insert(Diff_tree *dtree, long long *index_after);
+tree_code        tree_insert                        (Diff_tree *dtree, long long *index_after);
 
-tree_code        tree_insert_number(Diff_tree *dtree, long long *index_after, double number);   
-tree_code        tree_insert_variable(Diff_tree* dtree, long long* index_after, char variable);
-tree_code        tree_insert_operation(Diff_tree* dtree, long long* index_after, tree_operations operation);
+tree_code        tree_insert_number                 (Diff_tree *dtree, long long *index_after, double number);   
+tree_code        tree_insert_variable               (Diff_tree* dtree, long long* index_after, char variable);
+tree_code        tree_insert_operation              (Diff_tree* dtree, long long* index_after, tree_operations operation);
 
-static void      tree_remove_lief(Diff_tree* dtree, long long index_after, char answer);
-static void      tree_remove_branch(Diff_tree* dtree, long long branch_base);
-tree_code        tree_clean_branch(Diff_tree* dtree, long long branch_base);
-tree_code tree_remove_knot(Diff_tree* dtree, long long knot, long long next_knot);
+static void      tree_remove_lief                   (Diff_tree* dtree, long long index_after, char answer);
+static void      tree_remove_branch                 (Diff_tree* dtree, long long branch_base);
+tree_code        tree_clean_branch                  (Diff_tree* dtree, long long branch_base);
+tree_code        tree_remove_knot                   (Diff_tree* dtree, long long knot, long long next_knot);
 
-static tree_code tree_check_pointer(Diff_tree* dtree);
-static void      tree_check_connections(Diff_tree* dtree, long long index, tree_code* error);
+static tree_code tree_check_pointer                 (Diff_tree* dtree);
+static void      tree_check_connections             (Diff_tree* dtree, long long index, tree_code* error);
 
-void             tree_print_errors(tree_code code);
-void             tree_dump(Diff_tree* dtree, tree_code code, const tree_functions function);
-tree_code        tree_verifier(Diff_tree* dtree, const tree_functions requester);
+void             tree_print_errors                  (tree_code code);
+void             tree_dump                          (Diff_tree* dtree, tree_code code, const tree_functions function);
+tree_code        tree_verifier                      (Diff_tree* dtree, const tree_functions requester);
 
-void             tree_dot_call(const char* name_file, const char* expansion);
-void             tree_print_message_in_file(Diff_tree* dtree, long long index, FILE* picture);
-void             tree_print_picture_nodes(Diff_tree* dtree, long long index, FILE* picture);
-void             tree_generate_picture_nodes(Diff_tree* dtree, long long index, FILE* picture);
-void             tree_print_picture_sequence(Diff_tree* dtree, long long index, FILE* picture);
-void             tree_print_picture(Diff_tree* dtree, const char* picture_name);
+void             tree_dot_call                      (const char* name_file, const char* expansion);
+void             tree_print_message_in_file         (Diff_tree* dtree, long long index, FILE* picture);
+void             tree_print_picture_nodes           (Diff_tree* dtree, long long index, FILE* picture);
+void             tree_generate_picture_nodes        (Diff_tree* dtree, long long index, FILE* picture);
+void             tree_print_picture_sequence        (Diff_tree* dtree, long long index, FILE* picture);
+void             tree_print_picture                 (Diff_tree* dtree, const char* picture_name);
 
