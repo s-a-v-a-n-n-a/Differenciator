411afb35b274cd108aaec411514fe602916db4ef
s-a-v-a-n-n-a <70327418+s-a-v-a-n-n-a@users.noreply.github.com>
2020-12-14 16:00:35 +0300

    Updates

--- diff_differentiator.h
+++ diff_differentiator.h
@@ -6,19 +6,24 @@
 #include "Stack.h"
 #include "diff_work_with_file.h"
 #include "diff_tree.h"
+#include "tree_in_latex.h"
 
-long long diff_create_new_lief(Diff_tree* dtree, const char type, double future_value, long long left_index, long long right_index, long long parent_index);
-void diff(Diff_tree* old_tree, Diff_tree* dtree, const long long branch, long long current_index);
+const double epsilon = 0.000001;
 
-char diff_roll_up_consts(Diff_tree* dtree, long long index, char *changes);
+void			work_with_expression				();
 
-char diff_left_operations_with_zero(Diff_tree* dtree, long long index);
-char diff_right_operations_with_zero(Diff_tree* dtree, long long index);
-char diff_left_operations_with_one(Diff_tree* dtree, long long index);
-char diff_right_operations_with_one(Diff_tree* dtree, long long index);
-char diff_simplificate_operations(Diff_tree* dtree, long long index, char *changes);
+double			evaluate_expr						(Diff_tree* dtree, const long long index, double meaning);
 
-void diff_simplificate(Diff_tree* dtree);
+long long		diff_create_new_lief				(Diff_tree* dtree, const char type, double future_value, long long parent_index);
 
-void diff_print_tree_node(Diff_tree* dtree, long long index);
-void diff_print_tree_expression(Diff_tree* dtree, long long index);
+void			diff								(Diff_tree* old_tree, Diff_tree* dtree, const long long branch, long long current_index);
+
+char			diff_roll_up_consts					(Diff_tree* dtree, long long index, char *changes);
+
+char			diff_left_operations_with_zero		(Diff_tree* dtree, long long index);
+char			diff_right_operations_with_zero		(Diff_tree* dtree, long long index);
+char			diff_left_operations_with_one		(Diff_tree* dtree, long long index);
+char			diff_right_operations_with_one		(Diff_tree* dtree, long long index);
+char			diff_simplificate_operations		(Diff_tree* dtree, long long index, char *changes);
+
+void			diff_simplificate					(Diff_tree* dtree);
